c3fad6782ab71286ad38b3a6327143380d755d07
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-10-01 21:56:13 -0500

    Only freq slider added and gitignore file

--- underwater/underwater/ContentView.swift
+++ underwater/underwater/ContentView.swift
@@ -1,71 +1,9 @@
-//
-//  ContentView.swift
-//  underwater
-//
-//  Created by Stuart Ray on 9/9/23.
-//
 import SwiftUI
 
 struct ContentView: View {
-    // ViewModel instance
-    @ObservedObject var viewModel = SoundViewModel()
-    
     var body: some View {
-        ScrollView {
-            VStack {
-                // Amplitude Control
-                HStack {
-                    Text("Amplitude: \(viewModel.amplitude, specifier: "%.3f")")
-                    Slider(value: $viewModel.amplitude)
-                }
-                
-                // Frequency Control
-                HStack {
-                    Text("Frequency: \(Int(viewModel.frequency)) Hz")
-                    Slider(value: $viewModel.frequency)
-                }
-                
-                // Modulation
-                Picker("Modulation Type (\(viewModel.modulationType))", selection: $viewModel.modulationType) {
-                    Text("AM").tag("AM")
-                    Text("FM").tag("FM")
-                }
-                
-                // Pulse Duration & Interval
-                HStack {
-                    Text("Pulse Duration: \(viewModel.pulseDuration, specifier: "%.3f") s")
-                    Slider(value: $viewModel.pulseDuration)
-                }
-                
-                HStack {
-                    Text("Pulse Interval: \(viewModel.pulseInterval, specifier: "%.3f") s")
-                    Slider(value: $viewModel.pulseInterval)
-                }
-                
-                // Phase Control
-                HStack {
-                    Text("Phase: \(viewModel.phase, specifier: "%.3f") radians")
-                    Slider(value: $viewModel.phase)
-                }
-                
-                // Bandwidth Control
-                HStack {
-                    Text("Bandwidth: \(Int(viewModel.bandwidth)) Hz")
-                    Slider(value: $viewModel.bandwidth)
-                }
-                
-                // Equalization Control
-                HStack {
-                    Text("Equalization: \(viewModel.equalization, specifier: "%.3f")")
-                    Slider(value: $viewModel.equalization)
-                }
-                
-                // Start/Stop button
-                Button(viewModel.isPlaying ? "Stop" : "Start") {
-                    viewModel.togglePlaying()
-                }
-                
-            }.padding()
+        VStack {
+            SoundController()
         }
     }
 }
@@ -75,6 +13,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-
-
--- underwater/underwater/SoundController.swift
+++ underwater/underwater/SoundController.swift
@@ -1,44 +1,55 @@
 import SwiftUI
 import AVFoundation
 
+struct SoundController: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SoundController>) -> UIViewController {
+        let viewController = UIViewController()
+        let soundEngine = SoundEngine()
+        soundEngine.setup(in: viewController.view)
+        return viewController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<SoundController>) {
+        // Update code here, if needed
+    }
+}
+
 class SoundEngine {
     var audioEngine: AVAudioEngine!
     var sourceNode: AVAudioSourceNode!
     var isPlaying = false
-    var amplitude: Double = 0.5
     var frequency: Double = 440.0 // Frequency in Hz
-    var modulationType: String = "AM" // Placeholder for modulation type
-    var pulseDuration: Double = 1.0 // Placeholder for pulse duration
-    var pulseInterval: Double = 2.0 // Placeholder for pulse interval
-    var phase: Double = 0.0 // Placeholder for phase control
-    var bandwidth: Double = 20.0 // Placeholder for bandwidth control
-    var equalization: Double = 1.0 // Placeholder for equalization control
     
-    func updateParameters(from viewModel: SoundViewModel) {
-            self.amplitude = viewModel.amplitude
-            self.frequency = viewModel.frequency
-            self.modulationType = viewModel.modulationType
-            self.pulseDuration = viewModel.pulseDuration
-            self.pulseInterval = viewModel.pulseInterval
-            self.phase = viewModel.phase
-            self.bandwidth = viewModel.bandwidth
-            self.equalization = viewModel.equalization
-        }
-    
-    func generateWave(frame: Int, frameCount: AVAudioFrameCount) -> Int16 {
-            // Integrate amplitude, phase, and other variables here to generate the wave
-            let value = amplitude * sin(2.0 * .pi * frequency * Double(frame) / 44100.0 + phase)
-            let scaledValue = Int16(value * 32767.0)
-            return scaledValue
-        }
+    func generateWave(frequency: Double, frame: Int, frameCount: AVAudioFrameCount) -> Int16 {
+        let value = sin(2.0 * .pi * frequency * Double(frame) / 44100.0)
+        let scaledValue = Int16(value * 32767.0)
+        return scaledValue
+    }
+
     
     func setup(in view: UIView) {
         audioEngine = AVAudioEngine()
         
+        // Slider
+        let slider = UISlider(frame: CGRect(x: 20, y: 100, width: 280, height: 20))
+        slider.minimumValue = 20
+        slider.maximumValue = 2000
+        slider.value = 440
+        slider.addTarget(self, action: #selector(sliderChanged(_:)), for: .valueChanged)
+        view.addSubview(slider)
+        
+        // Button
+        let button = UIButton(frame: CGRect(x: 120, y: 200, width: 80, height: 40))
+        button.setTitle("Play", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.addTarget(self, action: #selector(togglePlay(_:)), for: .touchUpInside)
+        view.addSubview(button)
+        
         sourceNode = AVAudioSourceNode { _, _, frameCount, audioBufferList -> OSStatus in
             let ablPointer = UnsafeMutableAudioBufferListPointer(audioBufferList)
             for frame in 0..<Int(frameCount) {
-                let scaledValue = self.generateWave(frame: frame, frameCount: frameCount)
+                let scaledValue = self.generateWave(frequency: self.frequency, frame: frame, frameCount: frameCount)
                 for buffer in ablPointer {
                     let buf: UnsafeMutableBufferPointer<Int16> = UnsafeMutableBufferPointer(buffer)
                     buf[frame] = scaledValue
@@ -53,23 +64,18 @@ class SoundEngine {
         frequency = Double(sender.value)
     }
     
-    func startPlaying() {
-        if !isPlaying {
-            guard let format = AVAudioFormat(standardFormatWithSampleRate: 44100, channels: 1),
-                  let mixer = audioEngine?.mainMixerNode else {
-                return
-            }
-            audioEngine?.attach(sourceNode)
-            audioEngine?.connect(sourceNode, to: mixer, format: format)
-            try? audioEngine?.start()
-            isPlaying = true
+    @objc func togglePlay(_ sender: UIButton) {
+        if isPlaying {
+            audioEngine.stop()
+            sender.setTitle("Play", for: .normal)
+        } else {
+            let format = AVAudioFormat(standardFormatWithSampleRate: 44100, channels: 1)!
+            let mixer = audioEngine.mainMixerNode
+            audioEngine.attach(sourceNode)
+            audioEngine.connect(sourceNode, to: mixer, format: format)
+            try? audioEngine.start()
+            sender.setTitle("Stop", for: .normal)
         }
+        isPlaying = !isPlaying
     }
-    
-    func stopPlaying() {
-            if isPlaying {
-                audioEngine.stop()
-                isPlaying = false
-            }
-        }
 }
