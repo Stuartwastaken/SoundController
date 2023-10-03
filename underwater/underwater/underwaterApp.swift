91789494da8801b20f3c9fbcd0b6fc23b27581ad
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-10-02 22:05:50 -0500

    Added microphone permissions

--- underwater/underwater/underwaterApp.swift
+++ underwater/underwater/underwaterApp.swift
@@ -1,7 +1,18 @@
 import SwiftUI
+import AVFAudio
 
 @main
 struct underwaterApp: App {
+    init() {
+            // Request microphone permission
+            AVAudioSession.sharedInstance().requestRecordPermission { (granted) in
+                if granted {
+                    print("Microphone access granted")
+                } else {
+                    print("Microphone access denied")
+                }
+            }
+        }
     var body: some Scene {
         WindowGroup {
             ContentView()
