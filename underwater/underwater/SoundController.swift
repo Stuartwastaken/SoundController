4ace1fa07efee0acb89f86fcac9aedf19a322d3f
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-11-12 10:46:11 -0600

    Updated sounController

--- underwater/underwater/SoundController.swift
+++ underwater/underwater/SoundController.swift
@@ -4,12 +4,11 @@ import UIKit
 
 struct SoundController: UIViewControllerRepresentable {
     @Binding var frequency: Double
-        @Binding var chirpRate: Double
-        @Binding var time: Float
-        @Binding var spreadingFactor: Int
-        var onFrequencyTimeChange: ((Double, Float) -> Void)?
-    
-    
+    @Binding var chirpRate: Double
+    @Binding var time: Float
+    @Binding var spreadingFactor: Int
+    var onFrequencyTimeChange: ((Double, Float) -> Void)?
+
     func makeUIViewController(context: Context) -> SoundViewController {
         let controller = SoundViewController()
         controller.frequency = frequency
@@ -19,7 +18,7 @@ struct SoundController: UIViewControllerRepresentable {
         controller.onFrequencyTimeChange = self.onFrequencyTimeChange
         return controller
     }
-    
+
     func updateUIViewController(_ uiViewController: SoundViewController, context: Context) {
         uiViewController.frequency = frequency
         uiViewController.chirpRate = chirpRate
@@ -27,50 +26,49 @@ struct SoundController: UIViewControllerRepresentable {
         uiViewController.spreadingFactor = spreadingFactor
         uiViewController.onFrequencyTimeChange = self.onFrequencyTimeChange
     }
-
 }
 
 class SoundViewController: UIViewController {
-    var frequency: Double = 100.0
+    var frequency: Double = 0.0
     var chirpRate: Double = 50.0
-    var time: Float = 1.0
+    var time: Float = 0.01
     var spreadingFactor: Int = 12
     var onFrequencyTimeChange: ((Double, Float) -> Void)?
-    
     var audioEngine: AVAudioEngine!
     var sourceNode: AVAudioSourceNode!
     var isPlaying = false
     var timer: Timer?
-    
-    override func viewDidLoad() {
-            super.viewDidLoad()
-            setup()
+    var phaseAccumulator: Double = 0.0
+    var frequencyAccumulator: Double = 0.0
+    let frequencyIncrement: Double = 10
+    var chirpDuration: Double = 4
 
-            // Initialize the Timer to modulate frequency and update time
-        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
-                    let currentTime = Date().timeIntervalSince1970.truncatingRemainder(dividingBy: 5)
-                    self.frequency = (2200 * currentTime / 5)
-                    self.time = Float(currentTime)
-                    self.onFrequencyTimeChange?(self.frequency, self.time)
-                }
+    func generateChirp() -> Int16 {
+        phaseAccumulator += 2.0 * .pi * frequency / 44100.0
+        if phaseAccumulator > 2.0 * .pi {
+            phaseAccumulator -= 2.0 * .pi
         }
-    
-    // Generate the LoRa chirp
-    func generateChirp(frame: Int, frameCount: AVAudioFrameCount, spreadingFactor: Int) -> Int16 {
-        let fs: Double = 1000
-        let symbolTime: Double = pow(2, Double(spreadingFactor)) / fs
-        let t = Double(frame) * symbolTime / Double(frameCount)
-            let chirp = cos(2 * .pi * (frequency * t + 0.5 * chirpRate * pow(t, 2)))
-            return Int16(chirp * 32767.0)
+        let chirp = sin(phaseAccumulator)
+        return Int16(chirp * 32767.0)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.001, repeats: true) { _ in
+            let currentTime = Date().timeIntervalSince1970.truncatingRemainder(dividingBy: self.chirpDuration)
+            self.frequency = (0 + (22000 / (4 * self.chirpDuration)) * currentTime)
+            self.time = Float(currentTime)
+            self.onFrequencyTimeChange?(self.frequency, self.time)
         }
-    
+    }
+
     func setup() {
         audioEngine = AVAudioEngine()
-        
         sourceNode = AVAudioSourceNode { _, _, frameCount, audioBufferList -> OSStatus in
             let ablPointer = UnsafeMutableAudioBufferListPointer(audioBufferList)
             for frame in 0..<Int(frameCount) {
-                let chirp = self.generateChirp(frame: frame, frameCount: frameCount, spreadingFactor: self.spreadingFactor)
+                let chirp = self.generateChirp()
                 for buffer in ablPointer {
                     let buf: UnsafeMutableBufferPointer<Int16> = UnsafeMutableBufferPointer(buffer)
                     buf[frame] = chirp
@@ -78,27 +76,39 @@ class SoundViewController: UIViewController {
             }
             return noErr
         }
-        
-        // Add button for play/stop toggle
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playAndRecord, options: .defaultToSpeaker)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print("Error setting up audio session: \(error)")
+        }
         let button = UIButton(frame: CGRect(x: 120, y: 200, width: 80, height: 40))
         button.setTitle("Play", for: .normal)
         button.setTitleColor(.blue, for: .normal)
         button.addTarget(self, action: #selector(togglePlay(_:)), for: .touchUpInside)
         view.addSubview(button)
     }
-    
+
     @objc func togglePlay(_ sender: UIButton) {
-            if isPlaying {
-                audioEngine.stop()
-                sender.setTitle("Play", for: .normal)
-            } else {
-                let format = AVAudioFormat(standardFormatWithSampleRate: 44100, channels: 1)!
+        if isPlaying {
+            audioEngine.stop()
+            audioEngine.disconnectNodeOutput(sourceNode)
+            sender.setTitle("Play", for: .normal)
+        } else {
+            if let format = AVAudioFormat(standardFormatWithSampleRate: 44100, channels: 1) {
                 let mixer = audioEngine.mainMixerNode
                 audioEngine.attach(sourceNode)
                 audioEngine.connect(sourceNode, to: mixer, format: format)
-                try? audioEngine.start()
-                sender.setTitle("Stop", for: .normal)
+                do {
+                    try audioEngine.start()
+                    sender.setTitle("Stop", for: .normal)
+                } catch {
+                    print("Error starting audio engine: \(error)")
+                }
+            } else {
+                print("Cannot create AVAudioFormat")
             }
-            isPlaying = !isPlaying
         }
+        isPlaying = !isPlaying
+    }
 }
